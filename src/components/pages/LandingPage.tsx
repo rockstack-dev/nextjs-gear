8d27c436051eb84d49a9353ca8f3ad57d6ff3daa
AlexandroMtzG <alex.martinez@absys.com.mx>
2024-08-02 15:14:25 -0600

    landing page: font sizes

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -20,15 +20,15 @@ export default function LandingPage() {
     <div className="min-h-screen">
       <div className="mx-auto max-w-2xl space-y-6 p-4 py-8 sm:py-16">
         <div className="flex flex-col items-center justify-between gap-2 sm:flex-row">
-          <h1 className="text-xl font-bold sm:text-3xl">Next.js Gear 🦾</h1>
+          <h1 className="text-2xl font-bold sm:text-3xl">Next.js Gear 🦾</h1>
           <div className="flex flex-wrap items-center justify-center gap-2">
             <Button variant="ghost" asChild size="sm">
-              <Link href={YOUTUBE_URL}>
+              <Link href={YOUTUBE_URL} target="_blank">
                 <YouTubeIcon className="h-5 w-5 text-muted-foreground" />
               </Link>
             </Button>
             <Button variant="ghost" asChild size="sm">
-              <Link href={GITHUB_URL}>
+              <Link href={GITHUB_URL} target="_blank">
                 <GitHubIcon className="h-5 w-5 text-muted-foreground" />
               </Link>
             </Button>
@@ -38,40 +38,42 @@ export default function LandingPage() {
           </div>
         </div>
         <div className="space-y-1">
-          <div className="text-sm text-muted-foreground">
+          <div className="text-muted-foreground">
             The cleanest way to start a{" "}
-            <Link href="https://nextjs.org/" className="font-bold text-foreground hover:underline">
+            <Link href="https://nextjs.org/" className="font-bold text-foreground hover:underline" target="_blank">
               Next.js
             </Link>{" "}
             project with{" "}
-            <Link href="https://tailwindcss.com/" className="font-bold text-foreground hover:underline">
+            <Link href="https://tailwindcss.com/" className="font-bold text-foreground hover:underline" target="_blank">
               Tailwind CSS
             </Link>
             ,{" "}
-            <Link href="https://ui.shadcn.com/" className="font-bold text-foreground hover:underline">
+            <Link href="https://ui.shadcn.com/" className="font-bold text-foreground hover:underline" target="_blank">
               shadcn/ui
             </Link>
             , and{" "}
-            <Link href="https://www.i18next.com/" className="font-bold text-foreground hover:underline">
+            <Link href="https://github.com/sergiodxa/remix-i18next" className="font-bold text-foreground hover:underline" target="_blank">
               i18n
             </Link>
             .
           </div>
-          <div>
-            <pre className="relative truncate rounded-lg bg-secondary p-2 text-sm text-secondary-foreground">
+          <div className="space-y-2">
+            <pre className="relative truncate rounded-lg bg-secondary p-2 text-secondary-foreground">
               <button
-                className="absolute right-1 top-1 rounded-lg bg-primary p-1 text-sm text-primary-foreground"
+                className="absolute right-1 top-1 rounded-lg bg-primary p-1 text-primary-foreground"
                 onClick={() => {
                   navigator.clipboard.writeText(`git clone ${GITHUB_URL}`);
-                  toast({ title: "Copied to clipboard" });
+                  toast({
+                    title: "Copied to clipboard",
+                  });
                 }}
               >
                 {t("copy")}
               </button>
-              <code className="text-xs">git clone {GITHUB_URL}</code>
+              <code className="text-sm">git clone {GITHUB_URL}</code>
             </pre>
-            <div className="mt-1 flex justify-between text-xs text-muted-foreground">
-              <div className="flex items-center space-x-2 font-medium" style={{ fontSize: "0.6rem" }}>
+            <div className="flex justify-between text-sm text-muted-foreground">
+              <div className="flex items-center space-x-2 font-medium">
                 <Link href="https://remix-gear.vercel.app/" className="hover:underline">
                   Remix
                 </Link>
@@ -80,7 +82,7 @@ export default function LandingPage() {
                   Next.js
                 </Link>
               </div>
-              <Link href="https://saasrock.com" className="hover:underline" target="_blank">
+              <Link href="https://saasrock.com/?ref=remix-gear" className="text-muted-foreground hover:underline" target="_blank">
                 {t("sponsoredBy")} <span className="font-bold">SaasRock</span>
               </Link>
             </div>
