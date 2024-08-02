c3a5b4651e51b3c8df1d4f7da4fe3fd4258d4995
AlexandroMtzG <alex.martinez@absys.com.mx>
2024-08-02 15:59:54 -0600

    forms: use toast from sonner

--- src/components/FormSubmission.tsx
+++ src/components/FormSubmission.tsx
@@ -5,21 +5,17 @@ import clsx from "clsx";
 import { useActionState, useRef, useEffect } from "react";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
-import { toast } from "./ui/use-toast";
+import { toast } from "sonner";
 
 export default function FormSubmission() {
   const [actionData, action, pending] = useActionState(formSubmissionAction, null);
   const formRef = useRef<HTMLFormElement>(null);
   useEffect(() => {
     if (actionData?.success) {
-      toast({
-        title: actionData.success,
-      });
+      toast.success(actionData.success);
       formRef.current?.reset();
     } else if (actionData?.error) {
-      toast({
-        title: actionData.error,
-      });
+      toast.error(actionData.error);
       formRef.current?.reset();
     }
   }, [actionData]);
