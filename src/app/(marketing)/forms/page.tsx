2fc1fc0ab7d3b54afaf2705d5bba61471744719b
AlexandroMtzG <alex.martinez@absys.com.mx>
2024-08-03 12:47:07 -0600

    forms: center

--- src/app/(marketing)/forms/page.tsx
+++ src/app/(marketing)/forms/page.tsx
@@ -3,7 +3,7 @@ import HomeBreadcrumb from "@/components/HomeBreadcrumb";
 
 export default function () {
   return (
-    <div className="space-y-3 p-12">
+    <div className="mx-auto max-w-2xl space-y-3 p-12">
       <HomeBreadcrumb title="Form Actions" />
       <h1 className="text-3xl font-bold">Next.js Form Actions</h1>
       <div>
