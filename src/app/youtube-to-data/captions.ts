c6dde88947572729117a97b8fafcc3ad080b4bf9
AlexandroMtzG <alex.martinez@absys.com.mx>
2024-10-09 13:08:48 -0600

    try youtubei.js

--- src/app/youtube-to-data/captions.ts
+++ src/app/youtube-to-data/captions.ts
@@ -1,18 +1,41 @@
 import { YoutubeTranscript } from "youtube-transcript";
+import { Innertube } from "youtubei.js/web";
 
 export async function captions({ videoUrl }: { videoUrl: string }) {
   try {
-    const trascript = await YoutubeTranscript.fetchTranscript(videoUrl)
-      .then((data) => {
-        return data;
-      })
-      .catch((e) => {
-        throw new Error(`Could not get video transcript: ${e.message}`);
-      });
-    console.log({
-      transcriptLength: trascript.length,
+    const youtube = await Innertube.create({
+      lang: "en",
+      location: "US",
+      retrieve_player: false,
     });
-    return cleanText(trascript.map((t) => t.text).join(" "));
+
+    try {
+      const info = await youtube.getInfo(videoUrl);
+      const transcriptData = await info.getTranscript();
+      console.log({
+        transcriptData,
+      });
+      const transcriptSnippets = transcriptData.transcript.content?.body?.initial_segments.map((segment) => segment.snippet.text || "") || [];
+      console.log({
+        transcriptSnippets,
+      });
+      return cleanText(transcriptSnippets.join(" "));
+    } catch (error) {
+      console.error("Error fetching transcript:", error);
+      throw error;
+    }
+
+    // const trascript = await YoutubeTranscript.fetchTranscript(videoUrl)
+    //   .then((data) => {
+    //     return data;
+    //   })
+    //   .catch((e) => {
+    //     throw new Error(`Could not get video transcript: ${e.message}`);
+    //   });
+    // console.log({
+    //   transcriptLength: trascript.length,
+    // });
+    // return cleanText(trascript.map((t) => t.text).join(" "));
   } catch (err: any) {
     console.error("Error fetching video info:", err);
     throw err;
